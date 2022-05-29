cabeea0a42b9382e3871f90b6242e245f8e3b1a7
jyh7a <jyh7a@naver.com>
2022-05-29 17:18:07 +0900

    API_URL수정

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,1 +1,4 @@
-export const API_URL = "https://young-market-server.herokuapp.com";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://young-market-server.herokuapp.com"
+    : "http://localhost:8080";
